d0cf14f38dccbd05e7ff84ab84ae1cb768f94439
aredeeady-byte <aredeeady@gmail.com>
2026-06-09 15:24:30 +0800

    app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score
-import matplotlib as plt
+import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.metrics import confusion_matrix
 
